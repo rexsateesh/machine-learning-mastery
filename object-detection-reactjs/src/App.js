ad1fea9ecb8532f4f5af1dff3e01f763634ec594
Sateesh <salonora.srs@gmail.com>
2019-09-05 19:20:32 +0530

    Object detection with reactjs using tensorflow

---
+++ object-detection-reactjs/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RealTimeObjectDetection from './machine-learning/RealTimeObjectDetection'
+
+function App() {
+  return (
+    <div className="App">
+      <RealTimeObjectDetection />
+    </div>
+  );
+}
+
+export default App;
